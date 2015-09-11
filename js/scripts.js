c7590608c8b8da723a1034ea53b64102cfc541f0
marvinick <marviluw@gmail.com>
2015-09-11 10:05:41 -0700

    this commit will have passing first test

---
+++ js/scripts.js
@@ -0,0 +1,47 @@
+var findReplace = function(input) {
+  return input;
+}
+
+
+
+
+// var romanNumeral = function(number) {
+
+//   var list = { "M":1000,
+//               "CM":900,
+//                "D":500,
+//               "CD":400,
+//                "C":100,
+//               "XC":90,
+//                "L":50,
+//               "XL":40,
+//                "X":10,
+//               "IX":9,
+//                "V":5,
+//               "IV":4,
+//                "I":1
+//               };
+//     var roman = '';
+//     var i;
+
+//     for ( i in list ) {
+//       while ( number >= list[i] ) {
+//         roman += i;
+//         number -= list[i];
+//      }
+//     }
+//     return roman;
+// }
+
+// $(document).ready(function() {
+//   $("form#countupby").submit(function(event) {
+//     var number = parseInt($("input#number").val());
+//     var roman = romanNumeral(number);
+
+//     $(".number").text(number);
+//     $(".result").text(roman);
+
+//     $("#result").show();
+//     event.preventDefault();
+//   });
+// });
--- spec/specs.js
+++ spec/specs.js
@@ -1,4 +1,24 @@
 
 describe('findReplace', function() {
-    it("checks if the user able to input a word")
+  it("checks if the user able to input a word", function() {
+    expect(findReplace("and")).to.equal("and");
   })
+})
+
+
+// describe('romanNumeral', function() {
+//   it("checks if single digit is turned into roman", function() {
+//     expect(romanNumeral(5)).to.equal('V');
+//   })
+//   it("checks if double digits are turned into roman", function() {
+//     expect(romanNumeral(77)).to.equal("LXXVII");
+//   })
+//   it("checks if double digits are turned into roman", function() {
+//     expect(romanNumeral(999)).to.equal("CMXCIX");
+//   })
+
+//   // it("checks if count_by does not exceed equal to", function() {
+//   // expect(countUpBy(10,104)).to.eql([10, 20, 30, 40, 50, 60, 70, 80, 90, 100]);
+//   // })
+
+// })
